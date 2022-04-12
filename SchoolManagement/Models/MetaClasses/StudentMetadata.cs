d91fb0de0adbd2c00a573ef34b3c22f2dd354a66
Muhammad Umer <Muhammad.Umer@tpsonline.com>
2022-04-13 02:57:00 +0500

    Add project files.

---
+++ SchoolManagement/Models/MetaClasses/StudentMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class StudentMetadata
+    {
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        //[StringLength(50)]
+        //[Display(Name = "Middle Name")]
+        //public string MiddleName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Date Of Enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        
+    }
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+}
---
+++ SchoolManagement/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using SchoolManagement.Models;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagement.Startup))]
+namespace SchoolManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        public void CreateRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //var user = new ApplicationUser
+            //{
+            //    UserName = "admin",
+            //    Email = "admin@umer.com",
+            //    BirthDate = DateTime.Now
+            //};
+
+            //var password = "password";
+
+            //var usr = UserManager.Create(user, password);
+
+            //if (usr.Succeeded)
+            //{
+            //    var result = UserManager.AddToRole(user.Id, "Admin");
+            //}
+            if (!RoleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                RoleManager.Create(role);
+                
+               
+            }
+
+
+            if (!RoleManager.RoleExists("Teacher"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                RoleManager.Create(role);
+            }
+
+
+            if (!RoleManager.RoleExists("Supervisor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Supervisor";
+                RoleManager.Create(role);
+            }
+        }
+    }
+}
